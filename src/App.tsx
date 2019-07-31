ae8c7dfbb71df01630042bb7f3321bd503c50906
Nico Lance Medina <nico@nlmedina.com>
2019-07-31 20:55:23 +0800

    Initial commit, top bar and layout kitchen sink

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from 'react';
+import { Cell, Grid, Row } from '@material/react-layout-grid';
+import TopAppBar, { TopAppBarIcon, TopAppBarRow, TopAppBarSection, TopAppBarTitle } from '@material/react-top-app-bar';
+import MaterialIcon from '@material/react-material-icon';
+import Button from '@material/react-button';
+import './App.scss';
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <TopAppBar>
+        <TopAppBarRow>
+          <TopAppBarSection align="start">
+            <TopAppBarIcon navIcon tabIndex={0}>
+              <MaterialIcon hasRipple icon="menu" onClick={() => console.log('click')} />
+            </TopAppBarIcon>
+            <TopAppBarTitle>Material Design Sample</TopAppBarTitle>
+          </TopAppBarSection>
+        </TopAppBarRow>
+      </TopAppBar>
+      <Grid>
+        <Row>
+          <Cell columns={4}>
+            <div>
+              <Button raised>Does this work?</Button>
+            </div>
+          </Cell>
+          <Cell columns={4}>Cricket</Cell>
+          <Cell columns={4}>StarCraft</Cell>
+        </Row>
+        <Row>
+          <Cell desktopColumns={4} order={2} phoneColumns={4} tabletColumns={4}>
+            Tennis
+          </Cell>
+          <Cell desktopColumns={4} order={3} phoneColumns={4} tabletColumns={4}>
+            Cricket
+          </Cell>
+          <Cell desktopColumns={4} order={1} phoneColumns={4} tabletColumns={4}>
+            StarCraft
+          </Cell>
+        </Row>
+        <Row>
+          <Cell columns={4}>
+            <Row>
+              <Cell desktopColumns={8} phoneColumns={2} tabletColumns={5}>
+                Tennis
+              </Cell>
+              <Cell desktopColumns={4} phoneColumns={2} tabletColumns={3}>
+                Cricket
+              </Cell>
+            </Row>
+          </Cell>
+          <Cell columns={4}> - </Cell>
+          <Cell columns={4}> - </Cell>
+        </Row>
+      </Grid>
+    </Fragment>
   );
-}
+};
 
 export default App;
